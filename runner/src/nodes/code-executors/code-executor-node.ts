531d95f2aac848686c0d771dee73bb541f933af5
Kevin Piacentini <kevinpiac@gmail.com>
2025-03-22 19:44:44 +0100

    🚧 Fix input binding for code executor + simpler tsconfig"

--- runner/src/nodes/code-executors/code-executor-node.ts
+++ runner/src/nodes/code-executors/code-executor-node.ts
@@ -7,13 +7,15 @@ import {
   NodeResultCallback,
   NodeType,
 } from "../types";
+import { MissingArgumentError } from "./nodejs-executor/executor.errors";
 import { Argument, NodejsExecutor } from "./nodejs-executor/nodejs-executor";
 
 interface CodeExecutorNodeDefinition {
   code: string;
   language: "typescript";
   inputs: Record<string, string>;
-  outputs: Record<string, string>;
+  expectedArguments: { id: string; label: string; type: string }[];
+  expectedOutputs: { id: string; label: string; type: string }[];
 }
 
 export class CodeExecutorNode implements INode {
@@ -29,15 +31,27 @@ export class CodeExecutorNode implements INode {
     },
   ): Promise<any> {
     try {
-      const { code } = definition as unknown as CodeExecutorNodeDefinition;
-      const resolvedInputs = inputs as Record<string, string>;
+      const { code, expectedArguments } =
+        definition as unknown as CodeExecutorNodeDefinition;
 
-      const args = Object.entries(resolvedInputs).map(([key, value]) => {
+      const sortedExpectedArgumentLabels = expectedArguments.map(
+        (arg) => arg.label,
+      );
+
+      const args = sortedExpectedArgumentLabels.map((label) => {
+        const matchingInput = inputs[label];
+
+        // TODO @val: you can add your error handling here
+        if (!matchingInput) {
+          const errorMessage = `Missing argument named ${label}. This is likely a mistake in your flow.`;
+          options.onNodeLog(nodeId, errorMessage);
+          throw new MissingArgumentError(errorMessage);
+        }
+
         return {
-          name: key,
-          // TODO @val: how do we know the type of the value?
+          name: label,
           type: "any",
-          value,
+          value: matchingInput,
         } as Argument;
       });
 
@@ -71,7 +85,6 @@ export class CodeExecutorNode implements INode {
           }
 
           Object.entries(parsedResult).forEach(([key, value]) => {
-            console.log("ON NODE RESULT", key, value);
             options.onNodeResult(nodeId, key, value, "any");
           });
           return parsedResult;
@@ -79,95 +92,8 @@ export class CodeExecutorNode implements INode {
           assertUnreachable(language);
       }
     } catch (error) {
-      console.error(error);
+      console.error("Unexpected error while executing code node", error);
       throw error;
     }
   }
 }
-
-// const codeExecutorNode = new CodeExecutorNode();
-
-// codeExecutorNode.execute(
-//   "1",
-//   {
-//     type: "code-executor",
-//     inputs: {},
-//     outputs: {},
-//   },
-//   {
-//     code: `import { z } from "zod";
-
-//     function main(myValue: any, myValue2: any, double: (num: number) => number, myArray: any[], myObjArray: any[], myNumber: number, myBoolean: boolean) {
-//         let i = 0;
-//         while (i < 5) {
-//             console.log("Hello, world!", i);
-//             i++;
-//         }
-
-//         console.log('ZOD', z);
-
-//         return {
-//             "coucou": true,
-//             "i": i,
-//             "z": {
-//                 "coucou": true,
-//                 "i": i,
-//             },
-//             "myValue": myValue,
-//             "doubled": double(10),
-//             "array": myArray,
-//             "objArray": myObjArray,
-//             "myNumber": myNumber,
-//             "myBoolean": myBoolean,
-//         }
-//       }`,
-//     language: "typescript",
-//     args: [
-//       {
-//         name: "myValue",
-//         type: "string",
-//         value: "test",
-//       },
-//       {
-//         name: "myValue2",
-//         type: "any",
-//         value: {
-//           coucou: true,
-//         },
-//       },
-//       {
-//         name: "double",
-//         type: "any",
-//         value: (num: number) => num * 2,
-//       },
-//       {
-//         name: "myArray",
-//         type: "any",
-//         value: [1, 2, 3],
-//       },
-//       {
-//         name: "myObjArray",
-//         type: "any",
-//         value: [{ i: 1 }, { i: 2 }],
-//       },
-//       {
-//         name: "myNumber",
-//         type: "number",
-//         value: 10,
-//       },
-//       {
-//         name: "myBoolean",
-//         type: "boolean",
-//         value: true,
-//       },
-//     ],
-//   },
-//   {
-//     onNodeLog: async (message) => {
-//       //   console.log("LOG", message);
-//     },
-//     onNodeResult: async (result) => {
-//       //   console.log("RESULT", result);
-//     },
-//   },
-// );
