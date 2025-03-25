7eb6e8df7207d024294629e9f5c85c979da84b75
Valensto <hi@valensto.com>
2025-03-25 22:07:18 +0100

    refactor(provider): add new credential provider types

--- backend/internal/application/domain/model/provider.go
+++ backend/internal/application/domain/model/provider.go
@@ -1,39 +1,20 @@
 package model
 
 type (
-	ProviderType  string
-	ProviderModel string
+	ProviderType string
 )
 
 func (t ProviderType) String() string {
 	return string(t)
 }
 
-func (t ProviderModel) String() string {
-	return string(t)
-}
-
 const (
 	ProviderTypeOpenAI    ProviderType = "openai"
 	ProviderTypeAnthropic ProviderType = "anthropic"
+	ProviderTypeMistral   ProviderType = "mistral"
+	ProviderTypeGroq      ProviderType = "groq"
+	ProviderTypeGemini    ProviderType = "gemini"
+	ProviderTypeDeepSeek  ProviderType = "deepseek"
 
-	ModelGPT4o     ProviderModel = "gpt-4o"
-	ModelGPT4oMini ProviderModel = "gpt-4o-mini"
-
-	ModelClaude37Sonnet ProviderModel = "claude-3-7-sonnet"
-	ModelClaude35Sonnet ProviderModel = "claude-3-5-sonnet"
-	ModelClaude35Haiku  ProviderModel = "claude-3-5-haiku"
+	ProviderTypeE2B ProviderType = "e2b"
 )
-
-//nolint:gochecknoglobals // map to be used in multiple files of this package
-// var providerModels = map[ProviderType]map[ProviderModel]struct{}{
-// 	ProviderTypeOpenAI: {
-// 		ModelGPT4o:     {},
-// 		ModelGPT4oMini: {},
-// 	},
-// 	ProviderTypeAnthropic: {
-// 		ModelClaude37Sonnet: {},
-// 		ModelClaude35Sonnet: {},
-// 		ModelClaude35Haiku:  {},
-// 	},
-// }
