20bcde34bd71dce43d17811672f4926e07aaad10
Valensto <hi@valensto.com>
2025-03-25 18:16:58 +0100

    refactor(docker): rename services to include 'supallm_' prefix

--- frontend/src/context/env/env.ts
+++ frontend/src/context/env/env.ts
@@ -1,11 +1,11 @@
 "use server";
 
 export async function getEnv() {
-  if (!process.env.NEXT_PUBLIC_SUPALLM_API_URL) {
-    throw new Error("NEXT_PUBLIC_SUPALLM_API_URL is missing");
+  if (!process.env.PUBLIC_SUPALLM_API_URL) {
+    throw new Error("PUBLIC_SUPALLM_API_URL is missing");
   }
 
   return {
-    SUPALLM_API_URL: process.env.NEXT_PUBLIC_SUPALLM_API_URL,
+    SUPALLM_API_URL: process.env.PUBLIC_SUPALLM_API_URL,
   };
 }
--- openapi/gen-client/client.ts
+++ openapi/gen-client/client.ts
@@ -1,6 +1,5 @@
 import { getAuthToken } from "@/actions";
 import { OpenAPI } from "./index";
-import { getEnv } from "@/context/env/env";
 
 /* We can overwrite the default configuration by exporting a getToken function.
  * This function will be called before each request to get a token.
@@ -16,4 +15,4 @@ export const getToken = async () => {
 };
 
 OpenAPI.TOKEN = getToken;
-OpenAPI.BASE = process?.env?.NEXT_PUBLIC_SUPALLM_API_URL ?? "undefined";
+OpenAPI.BASE = "http://supallm_api:80";
